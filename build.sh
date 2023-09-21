7c779d58e8bda99417268f825583149fef90d132
Sascha Willems <webmaster@saschawillems.de>
2023-09-21 19:20:59 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+rwd=$(pwd)
+
+git -C "vulkan-tutorial" pull || git clone https://github.com/KhronosGroup/Vulkan-Tutorial.git "vulkan-tutorial"
+git -C "vulkan-guide" pull || git clone https://github.com/KhronosGroup/Vulkan-Guide.git "vulkan-guide"
+git -C "vulkan-samples" pull || git clone https://github.com/KhronosGroup/Vulkan-Samples.git "vulkan-samples"
+
+cd vulkan-tutorial/antora
+make
+cd $rwd
+
+cd vulkan-guide
+make -f antora/Makefile clean setup
+cd $rwd
+
+cd vulkan-samples
+cmake -H"." -B"build/unix" -DVKB_GENERATE_ANTORA_SITE=ON
+cd $rwd
+
+npx antora antora-playbook.yml
